796cf912c841dbc4f3003a0154b4e0028fe8ada5
kinjalmicra <kinjal.c.micra@gmail.com>
2025-02-06 11:44:55 +0530

    ##What is JavaScript? Basic syntax and data types.

---
+++ app.js
@@ -0,0 +1,25 @@
+// Write a simple JavaScript program that prints "Hello, World!" to the console.
+
+console.log("Hello, World!");
+
+// Experiment with different data types (string, number, boolean, null, undefined) and log them to the console.
+
+// String
+let name = "Hello";
+console.log("String:", name); // Output: String:Hello
+
+// Number
+let age = 25;
+console.log("Number:", age); // Output: Number: 25
+
+// Boolean
+let isStudent = true;
+console.log("Boolean:", isStudent); // Output: Boolean: true
+
+// Null
+let emptyValue = null;
+console.log("Null:", emptyValue); // Output: Null: null
+
+// Undefined
+let unknown;
+console.log("Undefined:", unknown); // Output: Undefined: undefined
